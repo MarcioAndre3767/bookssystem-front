db4c2ddaac5a13d2fb97f0b6f888b0f86c3aa4b6
MarcioAndre3767 <marcioandre.ti1@gmail.com>
2021-05-15 11:22:01 -0300

    finalizando o método 'create'

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Categoria } from './categoria.model';
@@ -13,11 +14,31 @@ export class CategoriaService {
   //referente a url da API que está injetado do 'environment.ts'
   baseUrl: String = environment.baseUrl;
 
-  constructor(private Http: HttpClient) { }
+  constructor(private Http: HttpClient, private _snackBar: MatSnackBar) { }
 
+  //servico-buscando categoria
   findAll():Observable<Categoria[]>{
-    const url = `${this.baseUrl}/categorias` 
-
+    const url = `${this.baseUrl}/categorias`
     return this.Http.get<Categoria[]>(url)
   }
+
+
+  //servico-criando categoria
+  create(categoria: Categoria): Observable<Categoria> {
+    const url = `${this.baseUrl}/categorias`
+    return this.Http.post<Categoria>(url, categoria)
+  }
+
+  //mensagem-criando categoria com sucesso
+  mensagem(str: String): void {
+    this._snackBar.open(`${str}`, 'OK', {
+      horizontalPosition: 'right',
+      verticalPosition: 'bottom',
+      duration: 3000
+    })
+  }
+
+
+
+
 }
