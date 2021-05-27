d115b4ea2edd6b03bb2a08fd21f667a05f687826
MarcioAndre3767 <marcioandre.ti1@gmail.com>
2021-05-27 18:09:12 -0300

    implementado UPDATE para o Livro

--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -1,20 +1,43 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { livro } from './livro.model';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { livro } from "./livro.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LivroService {
+  baseUrl: String = environment.baseUrl;
 
-  baseUrl: String = environment.baseUrl
-
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar) {}
 
   findAllByCategoria(id_cat: String): Observable<livro[]> {
-    const url = `${this.baseUrl}/livros?categoria=${id_cat}`
-    return this.http.get<livro[]>(url)
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`;
+    return this.http.get<livro[]>(url);
+  }
+
+  findById(id: String):Observable<livro>{
+    const url = `{this.baseUrl}/livros/${id}`
+    return this.http.get<livro>(url)
+  }
+
+  update(livro: livro):Observable<livro> {
+    const url = `${this.baseUrl}/livros/${livro.id}`
+    return this.http.put<livro>(url, livro)
+  }
+
+  create(livro: livro, id_cat: String): Observable<livro> {
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`;
+    return this.http.post<livro>(url, livro);
+  }
+
+  mensagem(str: String): void {
+    this._snackBar.open(`${str}`, "OK", {
+      horizontalPosition: "right",
+      verticalPosition: "bottom",
+      duration: 3000,
+    });
   }
 }
